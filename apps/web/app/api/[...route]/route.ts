208d25068642b659d47fee9b237de57e3a93d6b5
HKRK8569 <kayann3021@gmail.com>
2024-12-08 01:39:49 +0900

    workspaceの設定

--- apps/web/app/api/[...route]/route.ts
+++ apps/web/app/api/[...route]/route.ts
@@ -1,9 +1,9 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-
+import { app as backend } from "@next-hono-example/backend";
 export const runtime = "edge";
 
-const app = new Hono().basePath("/api");
+const app = new Hono().basePath("/api").route("/", backend);
 
 app.get("/hello", (c) => {
   return c.json({
